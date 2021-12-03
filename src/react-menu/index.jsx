20c8939bfd1db755944ae7e7f1ce398323f59e24
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-12-03 16:23:48 +0530

    chore: add react import

--- src/react-menu/index.jsx
+++ src/react-menu/index.jsx
@@ -1,50 +1,52 @@
-import ReactDOM from 'react-dom';
-import ReactMenu from './Menu';
-import Menu from '../menu';
-import { injectItem } from '../utils';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactMenu from "./Menu";
+import Menu from "../menu";
+import { injectItem } from "../utils";
 
 export default class extends Menu {
-    constructor(editor, props) {
-        super()
-        this.props = props;
-        this.items = [];
-        this.position = [0, 0];
-        this.visible = false;
-        this.el = document.createElement('div');
-        editor.view.container.appendChild(this.el);
+  constructor(editor, props) {
+    super();
+    this.props = props;
+    this.items = [];
+    this.position = [0, 0];
+    this.visible = false;
+    this.el = document.createElement("div");
+    editor.view.container.appendChild(this.el);
 
-        this.render();
-    }
+    this.render();
+  }
 
-    addItem(title, onClick, path = []) {
-        injectItem(this.items, title, onClick, path);
-        this.render();
-    }
+  addItem(title, onClick, path = []) {
+    injectItem(this.items, title, onClick, path);
+    this.render();
+  }
 
-    show(x, y, args) {
-        this.position = [x, y];
-        this.args = args;
-        this.visible = true;
-        this.render();
-    }
+  show(x, y, args) {
+    this.position = [x, y];
+    this.args = args;
+    this.visible = true;
+    this.render();
+  }
 
-    hide() {
-        this.visible = false;
-        this.render();
-    }
+  hide() {
+    this.visible = false;
+    this.render();
+  }
 
-    render() {
-        ReactDOM.render(<ReactMenu
-            {...this.props}
-            args={this.args}
-            items={this.items}
-            position={this.position}
-            visible={this.visible}
-            onClose={() => this.hide()}
-        />, this.el)
-    }
+  render() {
+    ReactDOM.render(
+      <ReactMenu
+        {...this.props}
+        args={this.args}
+        items={this.items}
+        position={this.position}
+        visible={this.visible}
+        onClose={() => this.hide()}
+      />,
+      this.el
+    );
+  }
 }
 
-export {
-    Menu
-}
+export { Menu };
